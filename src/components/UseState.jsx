d5a5313bfadc97a6d74b55d736ac08fec61d26a7
birendra gupta <96100549+birendra20@users.noreply.github.com>
2022-06-29 21:00:26 +0530

    usestate

---
+++ src/components/UseState.jsx
@@ -0,0 +1,27 @@
+import { React } from "react";
+import { useState } from "react";
+
+const UseState = () => {
+  const [counter, setCounter] = useState(0);
+  const [input, setInput] = useState("empty");
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  return (
+    <>
+      <h1>useState</h1>
+      {counter}
+      <button onClick={increment}>increment</button>
+      <p>input</p>
+      <input onChange={handleInput} />
+      <p>{input}</p>
+    </>
+  );
+};
+
+export default UseState;
